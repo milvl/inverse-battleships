9c0204bac7a4083f6935fc6113ab43ed0bdb6c59
mvlach <mvlach@students.zcu.cz>
2024-11-13 14:31:43 +0100

    - SW update:   - Client:     - Added base for SettingsMenu
    # mvlach

---
+++ sw/client/src/graphics/menus/settings_menu.py
@@ -0,0 +1,17 @@
+from typing import Any, Dict
+from pygame.rect import Rect
+from graphics.viewport import Viewport
+
+
+class SettingsMenu(Viewport):
+    def __init__():
+        pass
+
+    def redraw(self):
+        pass
+    
+    def draw(self) -> Rect:
+        pass
+    
+    def update(self, events: Dict[str, Any]) -> Dict[str, Any]:
+        pass
