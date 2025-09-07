0cff61452f3a83f58361489055fafcdc6b55e9dd
mvlach <mvlach@students.zcu.cz>
2025-09-07 13:28:33 +0200

    fix: correct project root path resolution

--- client/src/util/path.py
+++ client/src/util/path.py
@@ -14,19 +14,34 @@ def get_project_root() -> str:
     :rtype: str
     """
 
+    client_dir_id = 'client'
+
     # get the current working directory
     cwd = os.getcwd()
     
     # if the current working directory is the client directory
-    if os.path.basename(cwd) == 'client':
+    if os.path.basename(cwd) == client_dir_id:
         return cwd
     else:
-        # the project has been compiled using PyInstaller
-        if hasattr(sys, '_MEIPASS'):
-            base_path = sys._MEIPASS  # temporary directory for the executable when using PyInstaller
-        # the project is being run as a script from any directory
+        # # the project has been compiled using PyInstaller
+        # if hasattr(sys, '_MEIPASS'):
+        #     base_path = sys._MEIPASS  # temporary directory for the executable when using PyInstaller
+        # # the project is being run as a script from any directory
+        # else:
+        #     base_path = os.path.relpath(os.path.join(__file__, '..', '..', '..'))
+        
+        # try to find the 'client' directory in the path
+        path_parts = cwd.split(os.sep)
+        if client_dir_id in path_parts:
+            client_index = path_parts.index(client_dir_id)
+            base_path = os.sep.join(path_parts[:client_index + 1])
+        
+        # else if the 'client' directory is in the current directory
+        elif client_dir_id in os.listdir(cwd):
+            base_path = os.path.join(cwd, client_dir_id)
+        
         else:
-            base_path = os.path.relpath(os.path.join(__file__, '..', '..', '..'))
+            raise FileNotFoundError(f"The program has been started from an unexpected directory: {cwd}. Launch the program from the 'client' directory.")
 
         return base_path
     
